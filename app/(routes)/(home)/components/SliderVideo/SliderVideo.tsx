ff0fa8013a29ca1e6b3d41a1a3c8949c2c2957a7
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-02-05 20:08:42 +0100

    feat(betis): Muchos cambios

--- app/(routes)/(home)/components/SliderVideo/SliderVideo.tsx
+++ app/(routes)/(home)/components/SliderVideo/SliderVideo.tsx
@@ -10,11 +10,17 @@ export function SliderVideo() {
   useEffect(() => {
     setIsClient(true);
   }, []);
-  
+
   return (
     <div className="relative w-full -[80vw] md_h-[56.25vw] lg:h-[45vw]">
       {isClient && (
-        <video autoPlay loop muted className="brightness-50 object-fill w-full h-[80vw] md:[56.25vw] lg:h-[45vw]" src="/videos/Interstellar.mp4" />
+        <video
+          autoPlay
+          loop
+          muted
+          className="brightness-50 object-fill w-full h-[80vw] md:[56.25vw] lg:h-[45vw]"
+          src="/videos/Interstellar.mp4"
+        />
       )}
       <div className="flex flex-col justify-end absolute w-full md:w-[36%] px-4 md:px-0 md:left-[4%] z-20 top-0 -bottom-7 md:bottom-[36%]">
         <div className="pt-24 md:pt-0">
@@ -22,7 +28,12 @@ export function SliderVideo() {
             Interstellar
           </h2>
           <p className="max-w-md mt-2 text-xs md:text-base">
-            Al ver que la vida en la Tierra está llegando a su fin, un grupo de exploradores dirigidos por el piloto Cooper (McConaughey) y la científica Amelia (Hathaway) emprende una misión que puede ser la más importante de la historia de la humanidad: viajar más allá de nuestra galaxia para descubrir algún planeta en otra que pueda garantizar el futuro de la raza humana.
+            Al ver que la vida en la Tierra está llegando a su fin, un grupo de
+            exploradores dirigidos por el piloto Cooper (McConaughey) y la
+            científica Amelia (Hathaway) emprende una misión que puede ser la
+            más importante de la historia de la humanidad: viajar más allá de
+            nuestra galaxia para descubrir algún planeta en otra que pueda
+            garantizar el futuro de la raza humana.
           </p>
           <div className="flex flex-col md:flex-row gap-4 mt-5">
             <Button size="lg" variant="secondary">
@@ -36,7 +47,7 @@ export function SliderVideo() {
           </div>
         </div>
       </div>
-      <div className="bg-transparent bg-no-repeat bg-contain w-full opacity-100 top-auto h-[14.7vw] -bottom-16 absolute bg-gradient-video"/>
+      <div className="bg-transparent bg-no-repeat bg-contain w-full opacity-100 top-auto h-[14.7vw] -bottom-16 absolute bg-gradient-video" />
     </div>
   );
 }
